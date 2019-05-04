bce25b89cd5afb2e8a420ace0e07357552c66361
Madalina-I <madalush_de_plush@yahoo.com>
2019-05-04 12:02:22 +0300

    crud operations

---
+++ Lab1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Lab1.Models
+{
+    public class ProductsDbSeeder
+    {
+
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any product
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Huawei P10Lite",
+                    Description = "White",
+                    Category = "Huawei",
+                    Price = 5.00
+                }
+
+            );
+            context.SaveChanges();
+        }
+    }
+}
